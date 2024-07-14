c1f799fc0182168c2318f622bf32ef6d8de8c1e8
bidibulle <remy.delepaule@gmail.com>
2024-07-14 21:24:28 +0200

    FIX: add missing column in colas' table migration

--- backend/database/migrations/2024_07_14_142450_create_colas_table.php
+++ backend/database/migrations/2024_07_14_142450_create_colas_table.php
@@ -15,8 +15,20 @@ return new class extends Migration
             $table->id();
             $table->foreignId('product_id')->constrained();
             $table->foreignId('product_type_id')->constrained();
-            $table->string('registry_number')->nullable();
-            $table->string('serial_number')->nullable();
+            $table->string('region');
+            $table->string('type');
+            $table->string('appellation')->nullable();
+            $table->double('degree');
+            $table->string('grape_variety')->nullable();
+            $table->string('ttb_id');
+            $table->string('permit_number');
+            $table->string('brand')->nullable();
+            $table->string('fanciful_name')->nullable();
+            $table->string('serial_number');
+            $table->timestamp('approved_at')->nullable();
+            $table->string('status');
+            // represents "CHERCHE COLA" column in the Excel "All colas" sheet
+            $table->string('source');
             $table->timestamps();
         });
     }
